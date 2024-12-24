40c2c46520600aba99bc192dee9ddc799614dfa8
logiCCyber <117324079+logiCCyber@users.noreply.github.com>
2024-12-24 17:41:35 -0500

    Add files via upload

---
+++ js/reg.js
@@ -0,0 +1,20 @@
+
+document.querySelector("#form").addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const response = await fetch("/login", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            firstName: document.getElementById("firstName").value,
+            lastName: document.getElementById("lastName").value,
+            password: document.getElementById("password").value,
+            salary: document.getElementById("salary").value,
+        })
+    });
+
+    const data = await response.json();
+    console.log(data);
+});
