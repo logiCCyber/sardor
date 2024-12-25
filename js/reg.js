77cf8545af778697984494ffbd3b2c7fa70108d1
logiCCyber <117324079+logiCCyber@users.noreply.github.com>
2024-12-24 22:35:08 -0500

    Update reg.js

--- js/reg.js
+++ js/reg.js
@@ -1,20 +1,20 @@
 console.log("asdfasdf");
-document.querySelector("#form").addEventListener("submit", async (e) => {
-    e.preventDefault();
+// document.querySelector("#form").addEventListener("submit", async (e) => {
+//     e.preventDefault();
 
-    const response = await fetch("/login", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-            firstName: document.getElementById("firstName").value,
-            lastName: document.getElementById("lastName").value,
-            password: document.getElementById("password").value,
-            salary: document.getElementById("salary").value,
-        })
-    });
+//     const response = await fetch("/login", {
+//         method: "POST",
+//         headers: {
+//             "Content-Type": "application/json",
+//         },
+//         body: JSON.stringify({
+//             firstName: document.getElementById("firstName").value,
+//             lastName: document.getElementById("lastName").value,
+//             password: document.getElementById("password").value,
+//             salary: document.getElementById("salary").value,
+//         })
+//     });
 
-    const data = await response.json();
-    console.log(data);
-});
+//     const data = await response.json();
+//     console.log(data);
+// });
