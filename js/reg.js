53d424638066b8e03ba741cd32ee6e41485889f9
logiCCyber <117324079+logiCCyber@users.noreply.github.com>
2024-12-27 11:15:44 -0500

    Update reg.js

--- js/reg.js
+++ js/reg.js
@@ -1,20 +1,39 @@
-console.log("asdfasdf");
-document.querySelector("#form").addEventListener("submit", async (e) => {
-    e.preventDefault();
+(() => {
+  'use strict'
 
-    const response = await fetch("/login", {
-        method: "POST",
-        headers: {
+  // Получаем все формы с классом .needs-validation
+  const forms = document.querySelectorAll('.needs-validation')
+
+  // Перебираем формы и добавляем обработчик события
+  Array.from(forms).forEach(form => {
+    form.addEventListener('submit', async (event) => {
+      // Останавливаем отправку формы, если она невалидна
+      if (!form.checkValidity()) {
+        event.preventDefault()
+        event.stopPropagation()
+      } else {
+        // Если форма валидна, выполняем асинхронный запрос
+        event.preventDefault(); // Останавливаем стандартную отправку формы
+
+        const response = await fetch("/login", {
+          method: "POST",
+          headers: {
             "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
+          },
+          body: JSON.stringify({
             firstName: document.getElementById("firstName").value,
             lastName: document.getElementById("lastName").value,
             password: document.getElementById("password").value,
             salary: document.getElementById("salary").value,
-        })
-    });
+          })
+        });
 
-    const data = await response.json();
-    console.log(data);
-});
+        const data = await response.json();
+        console.log(data);
+      }
+
+      // Добавляем класс для отображения ошибок валидации
+      form.classList.add('was-validated')
+    }, false)
+  })
+})();
